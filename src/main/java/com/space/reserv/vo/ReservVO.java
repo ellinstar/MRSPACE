a42c6caaa6785d4e6fe717d8886b9945df138203
bobpri <ns0b0927@gmail.com>
2018-12-20 13:23:14 +0900

    예약 입니다.

---
+++ src/main/java/com/space/reserv/vo/ReservVO.java
@@ -0,0 +1,20 @@
+package com.space.reserv.vo;
+
+import lombok.Data;
+
+@Data
+public class ReservVO {
+
+	private int res_Num;
+	private String res_Date;
+	private String resUseDate;
+	private int res_Amount;
+	private int res_State;
+	private String mem_Id;
+	private int sp_Num;
+
+	public ReservVO() {
+		super();
+	}
+
+}
